ff74a35e37b11075217587ea45182930fd52b501
Yong Bakos <ybakos@humanoriented.com>
2021-04-06 15:24:09 -0700

    Statement: Introducing htmlStatement and rendering.

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -74,10 +74,28 @@ function renderPlainText(data, plays) {
 
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
 function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
         { style: "currency", currency: "USD", minimumFractionDigits: 2 })
         .format(aNumber/100);
 }
 
-module.exports = statement;
+module.exports = {statement, htmlStatement};
--- javascript/test/statement.test.js
+++ javascript/test/statement.test.js
@@ -1,10 +1,16 @@
-const statement = require('../src/statement');
+const {statement, htmlStatement} = require('../src/statement');
 const fs=require('fs');
 
 test('example statement', () => {
     const invoice = JSON.parse(fs.readFileSync('test/invoice.json', 'utf8'));
     const plays = JSON.parse(fs.readFileSync('test/plays.json', 'utf8'));
     expect(statement(invoice, plays)).toMatchSnapshot();
+});
+
+test('example HTML statement', () => {
+    const invoice = JSON.parse(fs.readFileSync('test/invoice.json', 'utf8'));
+    const plays = JSON.parse(fs.readFileSync('test/plays.json', 'utf8'));
+    expect(htmlStatement(invoice, plays)).toMatchSnapshot();
 });
 
 test('statement with new play types', () => {
