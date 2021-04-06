be6d4869aee69000fd82f7227e14409a3f32907f
Yong Bakos <ybakos@humanoriented.com>
2021-04-06 16:23:11 -0700

    Clean up and reorder.

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -4,6 +4,16 @@ function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
+function renderPlainText(data) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+}
+
 function htmlStatement(invoice, plays) {
     return renderHtml(createStatementData(invoice, plays));
 }
@@ -20,19 +30,6 @@ function renderHtml(data) {
     result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
     result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
     return result;
-}
-
-function renderPlainText(data) {
-
-    let result = `Statement for ${data.customer}\n`;
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(data.totalAmount)}\n`;
-    result += `You earned ${data.totalVolumeCredits} credits\n`;
-    return result;
-
 }
 
 function usd(aNumber) {
